9ffa9aeb19dbef3c7ca7620e3306b57fe2b1b9e6
Lucas Jones <lucasejones+1@gmail.com>
2023-02-07 09:23:04 -0800

    dynamic bg and favicon swap
    I created a custom context to import all the images into an object rather than use individual import statements at the top of the component.
    
    With this object, I used inline styling to dynamically update the element's background image when the planet's data shows.
    
    I also updated the favicon to the rebel logo to match the project theme.

--- freeCodeCamp projects/quote-machine/src/PlanetBox.js
+++ freeCodeCamp projects/quote-machine/src/PlanetBox.js
@@ -1,6 +1,7 @@
 import Button from './Button.js';
 // import twittericon from './images/twittericon.png';
-import Naboo from './Naboo.jpg';
+// import yav from './images/Yavin IV.webp';
+// import './Naboo.jpg';
 import { useState, useEffect} from 'react';
 
 
@@ -48,16 +49,52 @@ function PlanetBox() {
 		keys[i] = keys[i].replace(/_/g, ' ');
 	}
 
-	// display a semitransparent background image of the planet described
-	// assign some slight and themed bg color swap depending on planet
-	// fix all the unstructured css
-	// fix the favicon to be something relevant
 
+
+
+
+
+	function imageImport(r) {
+		let images = {}
+		r.keys().forEach((item, index) => {
+			images[item.replace('./', '').split('.')[0]] = r(item);
+		});
+		return images
+	}
+     
+	const allImages = imageImport(require.context('./images/', false))
+
+	if (item.name === 'Yavin IV') {
+		item.name = 'Yavin-IV' 
+	} 
+
+	// console.log('all images', allImages)
+	// console.log('item name', item.name)
+	// console.log('correct!', allImages[item.name])
+    // console.log('yavin?', allImages['Yavin-IV'])
+
+	// the space in Yavin IV was causing strange behavior in which the image was correctly indexed from the allImages object, but was not displaying, despite all the other data doing so. Modifying the image filename to swap the space for a hyphen and changing the fetched item name resolves the issue, though surely there's a more robust approach with larger datasets.
+
+
+
+
+	// assign some slight and themed bg color swap depending on planet
+	// fix all the unstructured css names
+	// allow visit to any specific planet with the little bottom dots?
 
 	return(
-		<div id='planet-box'>
-			{/*<img className='planet-bg' src={Naboo} alt='Naboo'/>*/}
-			<h1 id='planet-title'>{item.name === 'Yavin IV' ? 'Yavin 4' : item.name}</h1>
+		// <div id='planet-box'>
+		<div id='planet-box'
+			style={{
+				background: 'radial-gradient(rgba(0, 0, 0, 0.1) 50%, rgba(0, 0, 0, 0.3) 90%)',
+				backgroundImage: 'url('+allImages[item.name]+')',
+				backgroundSize: 'cover'
+			}}
+		> 
+			{/*<img className='planet-bg' src={yav} alt={yav}/>*/}
+		{/*<img className='planet-bg' src={allImages[item.name]} alt={item.name}/>*/}
+		{/*<img className='planet-bg' src={allImages[item.name]} alt={item.name}/>*/}
+			<h1 id='planet-title'>{item.name === 'Yavin-IV' ? 'Yavin-iv' : item.name}</h1>
 
 			<div className='fetched'>
 				<div className='fetched-keys'>
